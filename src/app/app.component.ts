8b682e79d21084418b9eb8d86fedd041adbd20d9
Sebastien <sebastien.andre.seb@gmail.com>
2017-01-04 03:16:58 +0100

    finish main panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {ProjectsComponent} from "./projects/projects.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WelcomeComponent, ProjectsComponent]
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
